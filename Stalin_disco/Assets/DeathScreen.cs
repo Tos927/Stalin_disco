03d5edd467836feb4e0ccf7f1d7d766956c4c630
Tos927 <t.sautereau92@gmail.com>
2021-10-22 16:44:21 +0200

    Modification script deathScreen + gameObject DeathScreen

--- Stalin_disco/Assets/DeathScreen.cs
+++ Stalin_disco/Assets/DeathScreen.cs
@@ -1,21 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathScreen : MonoBehaviour
 {
     public GameObject deathScreen;
 
-    public static GameOverManager instance;
+    public static DeathScreen instance;
 
     public void OnPlayerDeath()
     {
-        deathScreen.setActive(true);
+        deathScreen.SetActive(true);
     }
 
     public void RetryButton()
     {
-
+        LoadScene(SceneManager.GetActiveScene().buldIndex);
+        deathScreen.SetActive(true);
     }
 
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
 }
