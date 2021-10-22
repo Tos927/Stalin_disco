4ff5b1fc7929104674911e5266b9d28e6d7737b0
Tos927 <t.sautereau92@gmail.com>
2021-10-22 16:32:55 +0200

    Revert "Création Pause Screen"
    This reverts commit fdbaad3258c7f2ddb4418c69990df59c1f22a488.

--- Stalin_disco/Assets/DeathScreen.cs
+++ Stalin_disco/Assets/DeathScreen.cs
@@ -1,44 +1,21 @@
-<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-=======
-using UnityEngine;
-using UnityEngine.SceneManagement;
->>>>>>> Stashed changes
 
 public class DeathScreen : MonoBehaviour
 {
     public GameObject deathScreen;
 
-<<<<<<< Updated upstream
     public static GameOverManager instance;
 
     public void OnPlayerDeath()
     {
         deathScreen.setActive(true);
-=======
-    public static DeathScreen instance;
-
-    public void OnPlayerDeath()
-    {
-        deathScreen.SetActive(true);
->>>>>>> Stashed changes
     }
 
     public void RetryButton()
     {
-<<<<<<< Updated upstream
 
-=======
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        deathScreen.SetActive(false);
-    }
-
-    public void QuitButton()
-    {
-        Application.Quit();
->>>>>>> Stashed changes
     }
 
 }
